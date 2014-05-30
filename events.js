11ceecb12e8891a27243f696aa83bbd0268d6087
widmogrod <widmogrod@gmail.com>
2014-05-30 18:47:10 +0200

    add events class

---
+++ events.js
@@ -0,0 +1,53 @@
+(function(root, factory) {
+    if (typeof exports === 'object') { // Node.js
+        module.exports.events = factory();
+    } else if (typeof define === 'function' && define.amd) { // Require.JS
+        define(factory);
+    } else { // Browser globals
+        root.jef = root.jef || {};
+        root.jef.events = factory();
+    }
+})(this, function() {
+    'use strict';
+
+    function events() {
+        this.events = {};
+    }
+
+    events.constructor = events;
+    events.prototype.eachEvent = function(name, func) {
+        name.split(/\s+/).forEach(function(name) {
+            this.events[name] = name in this.events ? this.events[name] : [];
+            func(name);
+        }.bind(this));
+    };
+    events.prototype.on = function(name, func) {
+        this.eachEvent(name, function(name) {
+            this.events[name].push(func);
+        }.bind(this));
+
+        return this;
+    };
+    events.prototype.off = function() {
+        var idx;
+        this.eachEvent(name, function(name) {
+            idx = this.events[name].indexOf(func);
+            if (-1 !== idx) {
+                this.events[name].splice(idx, 1);
+            }
+        }.bind(this));
+
+        return this;
+    };
+    events.prototype.trigger = function(name, args, context) {
+        this.eachEvent(name, function(name) {
+            this.events[name].forEach(function(func) {
+                func.apply(context || func, args);
+            });
+        }.bind(this));
+
+        return this;
+    };
+
+    return events;
+});
