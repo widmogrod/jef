6f8b2cb3ddc662564f394f2075ba3c2c1c1b2b6b
widmogrod <widmogrod@gmail.com>
2014-05-31 11:16:53 +0200

    add events::trigger test

--- test/events.js
+++ test/events.js
@@ -1,10 +1,13 @@
 var events = require('../events.js');
-var object;
+var object, context, args;
 var callback = function() {};
+var params = [1, 2, 'test'];
 
 describe('Events', function() {
     beforeEach(function() {
         object = new events();
+        args = [];
+        context = null;
     });
 
     describe('#construction', function(){
@@ -37,6 +40,26 @@ describe('Events', function() {
              object.off('a')
              object.events['a'].should.be.empty;
         });
-
+    })
+    describe('#trigger', function() {
+        it('should trigger event and callback should be triggered', function(done) {
+             object.on('a', done)
+             object.trigger('a');
+        });
+        it('should trigger event and callback should be triggered with arguments', function() {
+             object.on('a', function() {
+                 args = arguments;
+             });
+             object.trigger('a', params);
+             args.should.be.arguments;
+             args.should.be.containEql(params)
+        });
+        it('should trigger event and callback should be triggered with arguments and context', function() {
+             object.on('a', function() {
+                 context = this;
+             });
+             object.trigger('a', params, object);
+             context.should.be.exactly(object);
+        });
     })
 });
