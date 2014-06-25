25b5db5fe5ece93d38838163cd2aa79463da37fd
widmogrod <widmogrod@gmail.com>
2014-06-25 17:37:13 +0200

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                preserveComments: false
+            },
+            single: {
+                files: {
+                    'dist/functional.min.js': ['functional.js'],
+                    'dist/mathematical.min.js': ['mathematical.js'],
+                    'dist/domdiff.min.js': ['domdiff.js'],
+                    'dist/events.min.js': ['events.js'],
+                    'dist/stream.min.js': ['stream.js'],
+                    'dist/reactive.min.js': ['reactive.js']
+                }
+            },
+            integration: {
+                files: {
+                    'dist/integration/jquery.diffhtml.min.js': ['domdiff.js', 'integration/jquery.diffhtml.js'],
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task(s).
+    grunt.registerTask('build', ['uglify:single', 'uglify:integration']);
+
+};
