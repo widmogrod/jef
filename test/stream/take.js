716629778446aedd62e0cfbb04f60f94563fd176
widmogrod <widmogrod@gmail.com>
2014-12-06 18:18:38 +0100

    Take stream test

---
+++ test/stream/take.js
@@ -0,0 +1,44 @@
+require('amdefine/intercept');
+
+var Stream = require('../../src/stream/stream');
+var fromArray = require('../../src/stream/fromArray');
+var take = require('../../src/stream/take');
+var object, withArgs, called;
+
+var args = function(value) {
+    called++;
+    withArgs = value;
+};
+var argsStop = function(value) {
+    called++;
+    withArgs = value;
+    return Stream.stop;
+};
+
+describe('MapStream', function() {
+    beforeEach(function() {
+        object = take(2, fromArray([1, 2, 3]));
+        withArgs = [];
+        called = 0;
+    });
+
+    describe('#construction', function() {
+        it('should construct object instane of MapStream', function() {
+            object.should.be.an.instanceOf(Stream);
+        })
+    });
+    describe('#on', function() {
+        it('should register onValue', function() {
+            object.on(args);
+            called.should.be.eql(2);
+            // Last arg should be
+            withArgs.should.be.eql(2);
+        });
+        it('should register onValue', function() {
+            object.on(argsStop);
+            called.should.be.eql(1);
+            // Last arg should be
+            withArgs.should.be.eql(1);
+        });
+    });
+});
