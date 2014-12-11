ce8f9e0a17c1deb66719f8de10a73d42168a8e7b
widmogrod <widmogrod@gmail.com>
2014-12-11 20:42:58 +0100

    Add asynchronously tests for skip stream

--- test/stream/skip.js
+++ test/stream/skip.js
@@ -2,6 +2,7 @@ require('amdefine/intercept');
 
 var Stream = require('../../src/stream/stream');
 var fromArray = require('../../src/stream/fromArray');
+var timeout = require('../../src/stream/timeout');
 var skip = require('../../src/stream/skip');
 var object, withArgs, called;
 
@@ -41,6 +42,29 @@ describe('Stream.skip', function() {
                 // Last arg should be
                 withArgs.should.be.eql(3);
             });
-        })
+        });
+        describe('asynchronously', function() {
+            beforeEach(function() {
+                object = skip(timeout(fromArray([1, 2, 3, 4])), 2);
+                withArgs = [];
+                called = 0;
+            });
+            it('should register onValue', function() {
+                object.on(args);
+                setTimeout(function() {
+                    called.should.be.eql(2);
+                    // Last arg should be
+                    withArgs.should.be.eql(4);
+                }, 20);
+            });
+            it('should register onValue and stop', function() {
+                object.on(argsStop);
+                setTimeout(function() {
+                    called.should.be.eql(1);
+                    // Last arg should be
+                    withArgs.should.be.eql(3);
+                }, 20);
+            });
+        });
     });
 });
