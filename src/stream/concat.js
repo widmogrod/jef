66e0ff7b572fd2606a068410a006a6d74a95fce0
widmogrod <widmogrod@gmail.com>
2014-12-14 14:58:49 +0100

    Test for Stream.both

--- src/stream/concat.js
+++ src/stream/concat.js
@@ -6,7 +6,7 @@ define(['./stream', './both'], function(Stream, both) {
      * @return {Stream}
      */
     return function concat(stream) {
-        return new Stream(function(sinkValue, sinkError, sinkComplete) {
+        return new Stream(function(sinkValue, sinkError) {
             stream.on(function(value, next) {
                 if (Stream.streamable(value)) {
                     concat(both(value, next)).on(function(value, inner) {
