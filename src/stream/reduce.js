29d100e6d33ba667b30a440a49501eba793dae5c
widmogrod <widmogrod@gmail.com>
2014-12-14 21:49:33 +0100

    Additional test for Stream.reduce

--- src/stream/reduce.js
+++ src/stream/reduce.js
@@ -8,12 +8,12 @@ define(['./stream'], function(Stream) {
      * @return {Stream}
      */
     return function reduce(stream, fn, base) {
-        return new Stream(function(sinkValue, sinkError, sinkComplete) {
+        return new Stream(function(sinkValue, sinkError) {
             stream.on(function(value) {
                 base = fn(value, base);
             }, sinkError, function() {
                 sinkValue(base, Stream.stop);
-            }, sinkComplete);
+            })
         })
     }
 });
