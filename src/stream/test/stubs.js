0c974b98fc74637fcadd8219d0abc21a3c704435
widmogrod <widmogrod@gmail.com>
2015-01-05 14:03:42 +0100

    New stub arrayWithError

--- src/stream/test/stubs.js
+++ src/stream/test/stubs.js
@@ -2,7 +2,7 @@ define(['../stream'], function(Stream) {
     'use strict';
 
     // I know that this is not a stub, but I don't know right now how to name it properly
-    return {
+    var Stubs = {
         onValue: function() {},
         onValueAndStop: function() {
             return Stream.stop;
@@ -10,11 +10,19 @@ define(['../stream'], function(Stream) {
         onError: function() {},
         onComplete: function() {},
         thrownError: new Error('test'),
-        throwError: function() {
+        throwError: function throwError() {
             throw this.error;
         },
         addOne: function(value) {
             return value + 1;
         }
     };
+
+    Stubs.arrayWithError = {
+        get 0 () {
+            Stubs.throwError();
+        }
+    };
+
+    return Stubs;
 });
