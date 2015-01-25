84f4a2a091071fd82c5762ebbc49c7624b8332b7
widmogrod <widmogrod@gmail.com>
2015-01-25 19:05:24 +0100

    update timeout stream to work better with push stream

--- src/stream/timeout.js
+++ src/stream/timeout.js
@@ -7,17 +7,27 @@ define(['./stream'], function (Stream) {
      * @return {Stream}
      */
     return function timeout(stream, wait) {
-        var called = 0;
+        var called = 0, completeInterval;
         return new Stream(function(sinkValue, sinkError, sinkComplete) {
             stream.on(function(value) {
                 called++;
                 setTimeout(function() {
                     sinkValue(value);
-                    if (!--called) {
+                    --called;
+                }, wait);
+            }, sinkError, function() {
+                if (!called) {
+                    return sinkComplete();
+                }
+
+                // Wait for complete
+                completeInterval = setInterval(function() {
+                    if (!called) {
                         sinkComplete();
+                        clearInterval(completeInterval);
                     }
                 }, wait);
-            }, sinkError);
+            });
         });
     };
 });
