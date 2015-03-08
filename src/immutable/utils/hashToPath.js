381e7fdd2ed19797742af64aa59f3e10d0968c59
widmogrod <widmogrod@gmail.com>
2015-03-08 20:16:27 +0100

    update function docblock

--- src/immutable/utils/hashToPath.js
+++ src/immutable/utils/hashToPath.js
@@ -1,6 +1,10 @@
 define(['./nodeNumber'], function(nodeNumber) {
     'use strict';
 
+    /**
+     * @param {String} hash
+     * @return {Array}
+     */
     return function hashToPath(hash) {
         var i = hash.length -1, path = [];
 
