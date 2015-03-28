bf18f57191db180ba323f4793826508d3efd6707
widmogrod <widmogrod@gmail.com>
2015-03-28 19:06:52 +0100

    funcitonal/keys used in functional/each

--- src/functional/each.js
+++ src/functional/each.js
@@ -1,21 +1,22 @@
 define([
-    './isNumber'
-], function(isNumber) {
+    './isNumber',
+    './keys'
+], function(isNumber, keysF) {
     'use strict';
 
     /**
      * Apply function on each data element
      */
     return function each(data, func, start, end) {
-        var i = 0,
-            keys = Object.keys(data),
+        var i,
+            keys = keysF(data),
             length = keys.length,
             key;
 
         isNumber(start) && (i = (start < 0) ? length + start : start);
         isNumber(end) && (length = (end < 0) ? length + end : end);
 
-        for (; i < length; i++) {
+        for (i = 0; i < length; i++) {
             key = keys[i];
             func(data[key], key, data);
         }
